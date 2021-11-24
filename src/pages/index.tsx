746f97d73ad541102c722354c5486e328b0b336e
Rei-x <wszyzdgo@gmail.com>
2021-11-24 21:57:51 +0000

    Update video url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ const Home = () => (
         <h1>Wideo</h1>
         <Video
           className="mt-4 rounded"
-          url="https://www.youtube.com/embed/bpOSxM0rNPM"
+          url="https://www.youtube.com/embed/U7DxsesC0_c"
         />
       </Container>
     </Fade>
